5aad57886a8bdf1ec4def6c81623c46ac2bf9776
FabioMainato <fabiaochirindzamainato@gmail.com>
2025-09-26 13:32:29 +0200

    Update cors and qrcode functionality

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
+const storage = getStorage(app);
 
-export { auth, db, functions };
+export { auth, db, functions, storage };
